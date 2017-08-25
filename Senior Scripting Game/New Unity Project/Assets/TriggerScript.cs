a91bbfe527c5a2672ef932d496e3ff8ef53246d7
Xarplo <xarplo@gmail.com>
2017-08-24 20:33:28 -0600

    Trigger Demo
    God I hope I did this right

---
+++ Senior Scripting Game/New Unity Project/Assets/TriggerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+		print ("Enter");
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit () {
+		print ("Exit");
+	
+	}
+}
